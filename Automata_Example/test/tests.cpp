d33b8ae7b421a8e8592f6124872ff72541851db2
Luke Amos <luke.amos.24@ucl.ac.uk>
2025-01-31 16:12:16 +0000

    Writing some tests for the exceptions I implemented.

--- Automata_Example/test/tests.cpp
+++ Automata_Example/test/tests.cpp
@@ -16,3 +16,19 @@ TEST_CASE("Check that a simple automaton accepts", "[simple accept]")
     REQUIRE(only_As.Read("aaaaa"));
     REQUIRE(!only_As.Read("aaaabaaa"));
 }
+
+TEST_CASE("Test Out of Alphabet", "[out_of_alphabet]") {
+    vector<vector<int>> M{{0,1}, {1, 1}};
+    vector<int> S_A{0};
+    map<char, int> A{{'a', 0}, {'b', 1}};
+    Automaton only_As(A, M, S_A);
+
+    REQUIRE_THROWS(only_As.Read("zaaa"));
+}
+
+TEST_CASE("Test Bad Matrix", "[bad_matrix]") {
+    vector<vector<int>> M{{0,1,0}, {1, 1,1}};
+    vector<int> S_A{0};
+    map<char, int> A{{'a', 0}, {'b', 1}};
+    REQUIRE_THROWS( Automaton(A, M, S_A));
+}
